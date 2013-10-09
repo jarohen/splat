40631883bf8bd7e91082c25dd4e471b877f84648
James Henderson <james@pcr.me.uk>
2013-10-09 09:00:12 +0100

    Adding description etc to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject spa-template/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Leiningen template to create ClojureScript single page web applications."
+  :url "https://github.com/james-henderson/cljs-spa-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
