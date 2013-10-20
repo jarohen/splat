87665a13b647fd79661d886211443dae7858fa33
James Henderson <james@pcr.me.uk>
2013-10-20 18:34:24 +0100

    Checking the hash on the home page

--- src/leiningen/new/splat/cljs/home.cljs
+++ src/leiningen/new/splat/cljs/home.cljs
@@ -5,5 +5,6 @@
 (defn watch-hash! [!hash]
   (add-watch !hash :home-page
              (fn [_ _ _ hash]
-               (d/replace-contents! (sel1 :#content) (node [:h2 {:style {:margin-top :1em}}
-                                                            "Hello world from ClojureScript!"])))))
+               (when (= "#/" hash)
+                 (d/replace-contents! (sel1 :#content) (node [:h2 {:style {:margin-top :1em}}
+                                                              "Hello world from ClojureScript!"]))))))
