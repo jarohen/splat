ec8cf7be53021cac656bd721ebd271c998315f09
James Henderson <james@pcr.me.uk>
2013-12-08 17:05:18 +0000

    Adding CLJX to project.clj

--- src/leiningen/new/splat/project.clj
+++ src/leiningen/new/splat/project.clj
@@ -17,29 +17,41 @@
                  [org.clojure/clojurescript "0.0-2080"]
                  [org.clojure/tools.reader "0.8.0"]]
 
-  :plugins [[jarohen/lein-frodo "0.2.3"]
+  :plugins [[jarohen/lein-frodo "0.2.5"]
             [lein-cljsbuild "1.0.0"]
-            [lein-pdo "0.1.1"]]
+            [lein-pdo "0.1.1"]
+            [com.keminglabs/cljx "0.3.1"]]
 
   :frodo/config-resource "{{name}}-config.edn"
 
-  :source-paths ["src/clojure"]
+  :source-paths ["src/clojure" "target/generated/clj"]
 
   :resource-paths ["resources" "target/resources"]
 
-  :cljsbuild {:builds [{:source-paths ["src/cljs"]
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/generated/clj"
+                   :rules :clj}
+
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/generated/cljs"
+                   :rules :cljs}]}
+
+  :cljsbuild {:builds [{:source-paths ["src/cljs" "target/generated/cljs"]
                         :compiler {:output-to "target/resources/js/{{name}}.js"
                                    :output-dir "target/resources/js/"
                                    :optimizations :whitespace
                                    :pretty-print true
-                                   :source-map "target/resources/js/{{name}}.js.map"}}
 
-                       {:source-paths ["src/cljs"]
+                                   ;; uncomment for source-maps
+                                        ; :source-map "target/resources/js/{{name}}.js.map"
+                                   }}
+
+                       {:source-paths ["src/cljs" "target/generated/cljs"]
                         :id "prod"
                         :compiler {:output-to "target/resources/js/{{name}}.js"
                                    :optimizations :advanced
                                    :pretty-print false
                                    :externs ["externs/jquery.js"]}}]}
 
-  :aliases {"dev" ["pdo" "cljsbuild" "auto," "frodo"]
-            "start" ["do" "cljsbuild" "once" "prod," "trampoline" "frodo"]})
+  :aliases {"dev" ["pdo" "cljx" "auto," "cljsbuild" "auto," "frodo"]
+            "start" ["do" "cljx" "once," "cljsbuild" "once" "prod," "trampoline" "frodo"]})
