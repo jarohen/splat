0c24da3ca70bcaa7d03f53d13ebccb93d6421df4
James Henderson <james@jarohen.me.uk>
2014-01-30 08:43:52 +0000

    Better way of handling missing classpath entries - make dev. 0.1.26

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject splat/lein-template "0.1.25"
+(defproject splat/lein-template "0.1.261"
   :description "A Leiningen template to create ClojureScript single page web applications."
   :url "https://github.com/james-henderson/splat"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/splat.clj
+++ src/leiningen/new/splat.clj
@@ -11,15 +11,12 @@
               :sanitized (name-to-path name)}]
     (->files data
              ["project.clj" (render "project.clj" data)]
+             ["Makefile" (render "Makefile" data)]
              [".gitignore" (render "gitignore" data)]
-             ["target/resources/js/.ph" (render "ph")]
-             ["target/generated/clj/.ph" (render "ph")]
-             ["target/generated/cljs/.ph" (render "ph")]
-             ["src/cljx/.ph" (render "ph")]
              ["resources/{{name}}-config.edn" (render "resources/config.edn" data)]
              
              ["src/clojure/{{sanitized}}/handler.clj" (render "clj/handler.clj" data)]
              ["src/cljs/{{sanitized}}/cljs/app.cljs" (render "cljs/app.cljs" data)]
              ["externs/jquery.js" (render "externs/jquery.js")]))
   (println "Created!")
-  (println "You can run the application with `lein dev`"))
+  (println "You can run the application with `make dev`"))
