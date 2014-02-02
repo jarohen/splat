19fe66f04a3de7249589b676ef9231f7f1697c06
James Henderson <james@jarohen.me.uk>
2014-02-02 19:47:53 +0000

    Updating deps, 0.3.3

--- src/leiningen/new/splat/project.clj
+++ src/leiningen/new/splat/project.clj
@@ -8,21 +8,21 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
 
                  [ring/ring-core "1.2.0"]
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.4"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
 
-                 [prismatic/dommy "0.1.1"]
+                 [prismatic/dommy "0.1.2"]
 
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/clojurescript "0.0-2138"]
-                 [org.clojure/tools.reader "0.8.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/tools.reader "0.8.3"]
 
                  [jarohen/frodo-core "0.2.10"]]
 
   :plugins [[jarohen/lein-frodo "0.2.10"]
-            [lein-cljsbuild "1.0.0"]
+            [lein-cljsbuild "1.0.2"]
             [lein-pdo "0.1.1"]
-            [com.keminglabs/cljx "0.3.1"]
+            [com.keminglabs/cljx "0.3.2"]
             [lein-shell "0.4.0"]]
 
   :frodo/config-resource "{{name}}-config.edn"
