7df498886c179bb569c1e34a496b3c13d67feacc
James Henderson <james@jarohen.me.uk>
2014-04-03 21:17:39 +0100

    Updating for Frodo 0.3.0-rc2

--- src/leiningen/new/splat/cljs/app.cljs
+++ src/leiningen/new/splat/cljs/app.cljs
@@ -1,13 +1,13 @@
 (ns {{name}}.cljs.app
   (:require [dommy.core :as d]
-            clojure.browser.repl)
+            frodo.brepl)
   (:require-macros [dommy.macros :refer [node sel1]]))
 
 (enable-console-print!)
 
 (set! (.-onload js/window)
       (fn []
-        (d/replace-contents! (sel1 :#content)
+        (d/replace-contents! js/document.body
                              (node [:div.container
                                     [:h2 {:style {:margin-top :1em}}
                                      "Hello world from ClojureScript!"]]))))
