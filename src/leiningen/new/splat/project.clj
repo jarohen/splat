5e6bf1b191d706d6f41c3732d5bec096db39a890
James Henderson <james@jarohen.me.uk>
2014-11-04 22:32:06 +0000

    Updating Flow version, 0.4.9

--- src/leiningen/new/splat/project.clj
+++ src/leiningen/new/splat/project.clj
@@ -13,7 +13,7 @@
                  [garden "1.2.1"]
                  [ring-middleware-format "0.4.0"]
 
-                 [jarohen/flow "0.2.0-beta2"]
+                 [jarohen/flow "0.2.0-beta3"]
                  [jarohen/nomad "0.7.0"]
 
                  [org.clojure/clojurescript "0.0-2280"]
