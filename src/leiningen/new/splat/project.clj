12cabcb51c91e81451c8022201d8b6558256718f
James Henderson <james@jarohen.me.uk>
2014-12-04 13:08:25 +0000

    Bumping Clojure to 1.7.0-a4, 0.4.19

--- src/leiningen/new/splat/project.clj
+++ src/leiningen/new/splat/project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
 
                  [ring/ring-core "1.2.0"]
                  [compojure "1.1.6"]
